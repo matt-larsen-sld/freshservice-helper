9b0cf8c76395d4530b77dd2d33f7505d818f2224
Matt Larsen <matt.larsen@connorgp.com>
2021-05-06 18:14:18 -0600

    Get single asset tested

--- functional_tests/test_assets_endpoint.py
+++ functional_tests/test_assets_endpoint.py
@@ -1,6 +1,6 @@
 import os
+
 import pytest
-import json
 
 from src.fshelper import (
     Credential,
@@ -25,3 +25,11 @@ def test_get_all_assets(fs_credential_and_domain):
         for assets in assets_endpoint.get_all(query="include=type_fields"):
             all_assets.extend(assets)
     assert len(all_assets) > 0
+
+
+def test_get_single_asset(fs_credential_and_domain):
+    credential, domain = fs_credential_and_domain
+    with RequestService(credential, domain) as fs_req_service:
+        asset_endpoint = AssetsEndPoint(fs_req_service)
+        asset = asset_endpoint.get(1)
+    assert asset["asset"]["display_id"] == 1
