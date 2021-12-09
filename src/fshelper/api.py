258af76d2c8568b6d439d0566a79a7efc2432720
Matt Larsen <matt.larsen@connorgp.com>
2021-12-09 11:19:58 -0700

    Bump to 0.1.0

--- src/fshelper/api.py
+++ src/fshelper/api.py
@@ -8,8 +8,9 @@ logger = logging.getLogger(__name__)
 class Credential:
     """Facilitate creating a Basic authentication credential for the FreshService API.
 
-       https://api.freshservice.com/#authentication
-     """
+    https://api.freshservice.com/#authentication
+    """
+
     def __init__(self, username=None, password=None):
         """Constructor for a Credential object.
 
@@ -28,6 +29,7 @@ class RequestService:
     Use this class with a context manager or new_session() with a try, except, finally block with
     'RequestService.session.close() in the finally block.
     """
+
     def __init__(self, credential: Credential, domain: str):
         """Constructor for a RequestService object
 
